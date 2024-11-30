0fa015c09db658d380f39dc3d1dcf8eacb937c5b
Marius Amarinei <145297889+alexamarinei@users.noreply.github.com>
2024-11-30 16:24:43 +0200

    Add files via upload
    upload the files of the project .

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import 'normalize.css';
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="app">
+      {/* <Navbar/> */}
+      <div className='content'>
+      </div>
+    </div>
+    </Router>
+  )
+}
+
+export default App;
